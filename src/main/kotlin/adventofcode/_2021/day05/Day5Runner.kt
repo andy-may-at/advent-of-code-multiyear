01a2d53f48a2c2ed082db157da312ef3e31676f8
Andy May <Andy.May@autotrader.co.uk>
2021-12-10 20:09:26 +0000

    Day 5 - create files

---
+++ src/main/kotlin/adventofcode/_2021/day05/Day5Runner.kt
@@ -0,0 +1,26 @@
+package adventofcode._2021.day05
+
+import adventofcode.utils.DayRunner
+
+
+fun main() {
+    Day5Runner().printResults()
+}
+
+
+class Day5Runner(readTestFile: Boolean = false): DayRunner(year = 2021, dayNumber = 5, readTestFile = readTestFile) {
+
+
+    override fun part1(): Int {
+
+        return -999111
+    }
+
+    override fun part2(): Int {
+
+        return -999111
+    }
+
+}
+
+
---
+++ src/test/kotlin/adventofcode/_2021/day05/Day5RunnerTest.kt
@@ -0,0 +1,16 @@
+package adventofcode._2021.day05
+
+import adventofcode.testutils.DayRunnerTestWrapper
+import adventofcode.testutils.Expected
+
+class Day5RunnerTest: DayRunnerTestWrapper(
+    runnerProvider = { readTestFile -> Day5Runner(readTestFile) },
+    Expected(
+        part1Test = 5,
+        part1Answer = -1,
+        part2Test = -1,
+        part2Answer = -1,
+    ),
+) {
+
+}
