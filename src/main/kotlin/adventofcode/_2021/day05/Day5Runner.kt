521d70d27a1684d680b5b2f074bdf1d9cb6b83a3
Andy May <Andy.May@autotrader.co.uk>
2021-12-10 21:22:11 +0000

    Day 5 - Part 1 complete

--- src/main/kotlin/adventofcode/_2021/day05/Day5Runner.kt
+++ src/main/kotlin/adventofcode/_2021/day05/Day5Runner.kt
@@ -13,12 +13,88 @@ class Day5Runner(readTestFile: Boolean = false): DayRunner(year = 2021, dayNumbe
 
     override fun part1(): Int {
 
-        return -999111
+        val linesSeq = inputReader.getParsedSequenceFromInput { stringLine ->
+            CoordPair(stringLine.split(" -> ").map { Coord(it) })
+        }
+
+        val linesWithFromClosestToZero = linesSeq
+            .filter { it.isVertical || it.isHorizontal }
+            .map(CoordPair::withFromClosestToZeroZero)
+            .toList()
+
+        val maxExtentOfCoords = linesWithFromClosestToZero.reduce { maxCoords, line ->
+            val minX = minOf(maxCoords.from.x, line.from.x)
+            val minY = minOf(maxCoords.from.y, line.from.y)
+            val maxX = maxOf(maxCoords.to.x, line.to.x)
+            val maxY = maxOf(maxCoords.to.y, line.to.y)
+
+            CoordPair(Coord(minX, minY), Coord(maxX, maxY))
+        }
+
+        val gridWidth = 1 + maxExtentOfCoords.maxX - maxExtentOfCoords.minX
+        val gridDepth = 1 + maxExtentOfCoords.maxY - maxExtentOfCoords.minY
+
+        val grid: Array<IntArray> = Array(gridWidth) { IntArray(gridDepth) }
+
+        println("Max grid extent = $maxExtentOfCoords")
+        println("gridWidth = $gridWidth")
+        println("gridDepth = $gridDepth")
+
+        val minGridPoint = Coord(maxExtentOfCoords.minX, maxExtentOfCoords.minY)
+
+        linesWithFromClosestToZero.forEach { line ->
+            val adjustedLine = line.withOffsetApplied(minGridPoint)
+
+            for (x in adjustedLine.from.x .. adjustedLine.to.x) {
+                for (y in adjustedLine.from.y .. adjustedLine.to.y) {
+                    grid[y][x] += 1
+                }
+            }
+        }
+
+        grid.forEach { println (it.contentToString()) }
+
+        return grid.sumOf { gridLine -> gridLine.count { it >= 2 } }
     }
+
 
     override fun part2(): Int {
 
         return -999111
+    }
+
+
+
+    data class CoordPair (val from: Coord, val to: Coord) {
+        constructor(coords: List<Coord>): this(coords[0], coords[1])
+
+        val isHorizontal = from.y == to.y
+        val isVertical = from.x == to.x
+
+        val maxX = maxOf(from.x, to.x)
+        val minX = minOf(from.x, to.x)
+        val maxY = maxOf(from.y, to.y)
+        val minY = minOf(from.y, to.y)
+
+
+        fun withOffsetApplied(offsetCoord: Coord): CoordPair {
+            return CoordPair(from.minus(offsetCoord), to.minus(offsetCoord))
+        }
+
+        fun withFromClosestToZeroZero(): CoordPair {
+            return if((from.x + from.y) < (to.x + to.y)) this else CoordPair(to, from)
+        }
+    }
+
+
+    data class Coord(val x: Int, val y: Int) {
+        constructor(s: String): this(
+            s.trim().split(",")[0].toInt(),
+            s.trim().split(",")[1].toInt())
+
+        fun minus(other: Coord):Coord {
+            return Coord(x - other.x, y - other.y)
+        }
     }
 
 }
--- src/test/kotlin/adventofcode/_2021/day05/Day5RunnerTest.kt
+++ src/test/kotlin/adventofcode/_2021/day05/Day5RunnerTest.kt
@@ -7,7 +7,7 @@ class Day5RunnerTest: DayRunnerTestWrapper(
     runnerProvider = { readTestFile -> Day5Runner(readTestFile) },
     Expected(
         part1Test = 5,
-        part1Answer = -1,
+        part1Answer = 6397,
         part2Test = -1,
         part2Answer = -1,
     ),
